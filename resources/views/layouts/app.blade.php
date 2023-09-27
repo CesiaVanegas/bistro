46f70afbe83dc2bc45ea8e406c62931ef761fa2d
unknown <jjvanegas@innovacion.gob.sv>
2023-09-26 20:05:02 -0600

    Agregando estilo de opcion de notificacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,15 +65,11 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-coffe shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-coffe shadow-sm">
             <div class="container">
                 <a class="navbar-brand text-white" href="{{ url('/') }}">
                     {{ config('app.name', 'Le Café des mots') }}
                 </a>
-                {{-- @auth
-                    <a class="nav-link text-white" href="{{ route('productos.index') }}">Productos&nbsp;&nbsp;&nbsp; </a>
-                @endauth --}}
-
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -96,12 +92,12 @@
                             </li>
                         @endauth
 
-                          <!-- Enlace para "Bebidas" -->
-                          @auth
-                          <li class="nav-item">
-                              <a class="nav-link text-white" href="{{ route('bebidas.index') }}">Bebidas</a>
-                          </li>
-                      @endauth
+                        <!-- Enlace para "Bebidas" -->
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('bebidas.index') }}">Bebidas</a>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -125,11 +121,19 @@
                                     role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
                                     v-pre>
                                     {{ Auth::user()->name }}
+                                    <!-- Badge para Notificaciones -->
+                                    <span class="badge bg-danger">3</span>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <!-- Opción de Notificaciones -->
+                                    <a class="dropdown-item" href="#">
+                                        {{ __('Notificaciones') }}
+                                         <!-- Badge para Notificaciones -->
+                                    <span class="badge bg-danger">3</span>
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ __('Cerrar sesión') }}
                                     </a>
 
@@ -139,11 +143,109 @@
                                 </div>
                             </li>
                         @endguest
+
+                    </ul>
+                </div>
+            </div>
+        </nav> --}}
+        <nav class="navbar navbar-expand-md navbar-light bg-coffe shadow-sm">
+            <div class="container">
+                <a class="navbar-brand text-white" href="{{ url('/') }}">
+                    {{ config('app.name', 'Le Café des mots') }}
+                </a>
+        
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+        
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="{{ route('postres.index') }}">Postres</a>
+                        </li>
+                        @endauth
+        
+                        <!-- Enlace para "Libros" -->
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="{{ route('biblioteca.index') }}">Libros</a>
+                        </li>
+                        @endauth
+        
+                        <!-- Enlace para "Bebidas" -->
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="{{ route('bebidas.index') }}">Bebidas</a>
+                        </li>
+                        @endauth
+                    </ul>
+        
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Badge para Notificaciones -->
+                        <li class="nav-item dropdown">
+                            <div class="position-relative">
+                                <a class="nav-link text-white" href="#" data-bs-toggle="dropdown">
+                                    <i class="fa fa-bell"></i>
+                                    <span class="badge bg-danger badge-sm badge-pill position-absolute top-1 end-1 translate-middle" style="font-size: 9px;">
+                                        3
+                                    </span>
+                                </a>
+                                <!-- Menú desplegable de notificaciones -->
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <!-- Contenido del menú de notificaciones -->
+                                    <!-- Puedes agregar aquí el contenido de las notificaciones -->
+                                   
+                                    <a class="dropdown-item" href="#">Notificación 1</a>
+                                    <a class="dropdown-item" href="#">Notificación 2</a>
+                                    
+                                </div>
+                            </div>
+                        </li>
+                        
+        
+                        <!-- Authentication Links -->
+                        @guest
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
+        
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#"
+                                role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                                v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    {{ __('Cerrar sesión') }}
+                                </a>
+        
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
                     </ul>
                 </div>
             </div>
         </nav>
-
+        
         <main class="py-4">
             @yield('content')
         </main>
