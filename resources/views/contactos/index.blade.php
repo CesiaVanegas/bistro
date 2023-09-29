7f90274b655277e3995f07941c7fa663ef64b1ee
Jova <jjvanegas@innovacion.gob.sv>
2023-09-29 15:15:40 -0600

    Avance de notificaciones

--- resources/views/contactos/index.blade.php
+++ resources/views/contactos/index.blade.php
@@ -43,25 +43,8 @@
                 </tbody>
             </table>
         </div>
-        <div class="modal fade" id="myModal">
-            <div class="modal-dialog modal-md">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Información</h4>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p hidden><strong>id</strong> <span id="id"></span></p>
-                        <p><strong>Nombre:</strong> <span id="nombre"></span></p>
-                        <p><strong>Email:</strong> <span id="email"></span></p>
-                        <p><strong>Título:</strong> <span id="titulo"></span></p>
-                        <p><strong>Mensaje:</strong> <span id="mensaje"></span></p>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include ('contactos.modal')
+       
     </div>
 
-    <!-- {{ url(request()->path()) }} -->
 @endsection
---
+++ resources/views/contactos/modal.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Información</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                
+                <p hidden><strong>id</strong> <span id="id"></span></p>
+                <p><strong>Nombre:</strong> <span id="nombre"></span></p>
+                <p><strong>Email:</strong> <span id="email"></span></p>
+                <p><strong>Título:</strong> <span id="titulo"></span></p>
+                <p><strong>Mensaje:</strong> <span id="mensaje"></span></p>
+
+            </div>
+        </div>
+    </div>
+</div>
