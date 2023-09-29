f38d0089955af3e7db0f2c248a4064142ed941ca
Jova <jjvanegas@innovacion.gob.sv>
2023-09-29 15:57:37 -0600

    Avance de notificaciones

--- public/js/info.js
+++ public/js/info.js
@@ -1,27 +1,35 @@
-console.log('hola');
-const id_contacto = document.getElementById('id_contacto');
+// Obtén los elementos por sus IDs
+
+const id_contacto = document.getElementById('id');
 const nombre = document.getElementById('nombre');
 const email = document.getElementById('email');
-const titulo= document.getElementById('titulo');
-const mensaje= document.getElementById('mensaje');
+const titulo = document.getElementById('titulo');
+const mensaje = document.getElementById('mensaje');
+document.addEventListener('DOMContentLoaded', function () {
+function contacto(id_contacto = null) {
+if (id_contacto) {
+console.log(id_contacto);
+axios.get(url + '/' + id_contacto)
+.then(res => {
+const contacto = res.data.contacto;
+console.log(contacto.id);
+console.log(contacto.nombre);
+console.log(contacto.email);
+console.log(contacto.titulo);
+console.log(contacto.mensaje);
+console.log(contacto);
 
-function contacto(id_contacto=null){
-    if (id_contacto) {
-            console.log(id_contacto);
-        axios.get(url + '/' + id_contacto).then(res=>{
-            const contacto =res.data.contacto;
-            console.log(contacto.id); 
-            console.log(contacto.nombre);
-            console.log(contacto.email);
-            console.log(contacto.titulo);
-            console.log(contacto.mensaje);
-            console.log(contacto);
-                    
-        }).catch(error=>{
-            console.log(error);
-        });
-    }
-
+// Actualiza el contenido de los elementos por sus IDs
+id_contacto.innerText = contacto.id;
+nombre.innerText = contacto.nombre;
+email.innerText = contacto.email;
+titulo.innerText = contacto.titulo;
+mensaje.innerText = contacto.mensaje;
+})
+.catch(error => {
+console.log(error);
+});
 }
-
+}
 contacto();
+});
